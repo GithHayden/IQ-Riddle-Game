88068555df7827355e9108ccce2c1b70eee89433
Ann Hayden <hayden.ann@gmail.com>
2018-05-30 12:53:00 +0000

    Add page_heading to run.py & tempalte logic to html <h1> tags

--- run.py
+++ run.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     '''Routing view to render/call index.html in browser'''
-    return render_template("index.html")
+    return render_template("index.html", page_heading="Game")
 
 @app.route('/contact')
 def contact():
     '''Routing view to render/call contact.html in browser'''
-    return render_template("contact.html")
+    return render_template("contact.html", page_heading="Contact Developer")
 
 if __name__== '__main__':
     app.run(host=os.environ.get('IP'),
