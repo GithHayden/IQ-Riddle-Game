ace6bc83964f0d0ef4e457ee35390c9003df1f6c
Ann Hayden <hayden.ann@gmail.com>
2018-05-30 10:37:21 +0000

    Contact.html and relevant Run.py routing

--- run.py
+++ run.py
@@ -3,11 +3,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    '''Routing to render html template'''
+    '''Routing to render index.html in browser'''
     return render_template("index.html")
-    
+
+
+@app.route('/contact')
+def contact():
+    '''Routing to render contact.html in browser'''
+    return render_template("contact.html")
+
+
 if __name__== '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
